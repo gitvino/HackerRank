cb650c8629020d19ae757d33d830d5360d54d46e
Vinoth <vinoth@aleph-labs.com>
2019-06-13 15:06:35 +0800

    Complete Organizing Containers of Balls problem

--- Problem Solving/Organizing_Containers_of_Balls.py
+++ Problem Solving/Organizing_Containers_of_Balls.py
@@ -3,5 +3,39 @@
 URL: https://www.hackerrank.com/challenges/organizing-containers-of-balls
 
 '''
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the organizingContainers function below.
+def organizingContainers(container):
+    size = len(container)
+    row_sum = []
+    col_sum = []
+
+    for index in range(size):
+        col_sum.append(sum(row[index] for row in container))
+        row_sum.append(sum(container[index]))
+
+    row_sum.sort()
+    col_sum.sort()
+
+    if row_sum == col_sum:
+        return "Possible"
+    else:
+        return "Impossible"
+
 if __name__ == '__main__':
-    print("")
+
+    q = int(input())
+    for q_itr in range(q):
+        n = int(input())
+        container = []
+        for _ in range(n):
+            container.append(list(map(int, input().rstrip().split())))
+        result = organizingContainers(container)
+        print(result + '\n')
+
